db9e3faf8f77381ae58845dcf7a3fc71a7b4abf4
Michael Pak <michaelpak@MacBook-Pro-Michael.local>
2017-02-12 22:13:02 +0300

    initial commit #2

--- wedding/urls.py
+++ wedding/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
+
+from web import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-]
+    url(r'^$', views.index),
+    url(r'^archive/', admin.site.urls),
+    url(r'^single/', admin.site.urls),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
